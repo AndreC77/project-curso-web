0722a2fa21fe0affe4a0b2cd26b0fd6441f8aefe
Andre <andresantosgf@gmail.com>
2021-03-19 11:26:55 -0300

    Exception handling - findById/delete

--- src/main/java/com/andrecoelho/webproject/services/UserService.java
+++ src/main/java/com/andrecoelho/webproject/services/UserService.java
@@ -4,10 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.andrecoelho.webproject.entities.User;
 import com.andrecoelho.webproject.repositories.UserRepository;
+import com.andrecoelho.webproject.services.exceptions.DataBaseException;
+import com.andrecoelho.webproject.services.exceptions.ResourceNotFoundExceptions;
 
 @Service
 public class UserService {
@@ -21,7 +25,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundExceptions(id));
 	}
 
 	public User insert(User obj) {
@@ -30,7 +34,14 @@ public class UserService {
 	}
 
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundExceptions(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataBaseException(e.getMessage());
+		}
+		
 	}
 
 	public User update(Long id, User obj) {
